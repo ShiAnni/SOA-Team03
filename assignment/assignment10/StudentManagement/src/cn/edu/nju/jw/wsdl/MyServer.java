8f20c9c22d4c4e9939ace84d30604b96efd95cf3
ShiAnni <shino.ann@outlook.com>
2017-06-09 23:46:14 +0800

    作业10.3准备工作

--- assignment/assignment10/StudentManagement/src/cn/edu/nju/jw/wsdl/MyServer.java
+++ assignment/assignment10/StudentManagement/src/cn/edu/nju/jw/wsdl/MyServer.java
@@ -4,7 +4,6 @@ import javax.xml.ws.Endpoint;
   
 public class MyServer {  
     public static void main(String[] args) {  
-        //访问方式：hhttp://localhost:8080/WS3
         String address="http://localhost:8080/assignment10/StudentManagement";  
         Endpoint.publish(address, new StudentManagementImpl());  
     }  
